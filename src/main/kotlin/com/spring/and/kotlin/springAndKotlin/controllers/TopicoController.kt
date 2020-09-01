790519850436d9fcf50db2e7818e5fc88c0a1550
Bruno Yudi Tiyoda <bruno_tiyoda@hotmail.com>
2020-09-01 20:27:23 -0300

    invalidate cache

--- src/main/kotlin/com/spring/and/kotlin/springAndKotlin/controllers/TopicoController.kt
+++ src/main/kotlin/com/spring/and/kotlin/springAndKotlin/controllers/TopicoController.kt
@@ -5,6 +5,7 @@ import com.spring.and.kotlin.springAndKotlin.controllers.dtos.response.TopicoRes
 import com.spring.and.kotlin.springAndKotlin.controllers.mappers.TopicoMapper
 import com.spring.and.kotlin.springAndKotlin.services.TopicoService
 import org.slf4j.LoggerFactory
+import org.springframework.cache.annotation.CacheEvict
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
@@ -23,7 +24,6 @@ class TopicoController(
 ) {
     private val logger = LoggerFactory.getLogger(TopicoController::class.java)
 
-
     @GetMapping
     @Cacheable(value = ["listaDeTopicos"])
     fun topicos(@PageableDefault(
@@ -33,6 +33,7 @@ class TopicoController(
             direction = Sort.Direction.ASC
     ) pageable: Pageable): Page<TopicoResponseDTO> {
         logger.info("find all topicos pageable. page number ${pageable.pageNumber} page size ${pageable.pageSize}")
+
         val findAll = topicoService.buscaTodosOsTopicos(pageable)
 
         return topicoMapper.toDTO(findAll)
@@ -41,6 +42,7 @@ class TopicoController(
     @GetMapping("/{id}")
     fun buscaPorUmTopico(@PathVariable id: Long): ResponseEntity<TopicoResponseDTO> {
         logger.info("find topico by id $id")
+
         val topico = topicoService.buscaUmTopico(id)
         val dto = topicoMapper.toDTO(topico)
 
@@ -50,6 +52,7 @@ class TopicoController(
     @GetMapping("/nomeCurso")
     fun filtraTopicosPorNomeDoCurso(@RequestParam nomeDoCurso: String): List<TopicoResponseDTO> {
         logger.info("filter topico by name $nomeDoCurso")
+
         val filtraTopicosPorNomeDoCurso =
                 topicoService.filtraTopicosPorNomeDoCurso(nomeDoCurso)
 
@@ -57,11 +60,13 @@ class TopicoController(
     }
 
     @PostMapping
+    @CacheEvict(value = ["listaDeTopicos"], allEntries = true)
     fun cadastrarNovoTopico(
             @RequestBody topicoRequestDTO: TopicoRequestDTO,
             uriBuilder: UriComponentsBuilder
     ): ResponseEntity<TopicoResponseDTO> {
         logger.info("new topico $topicoRequestDTO")
+
         val novoTopicoCadastrado = topicoService.cadastrarNovoTopico(topicoMapper.toDomain(topicoRequestDTO))
         val dto = topicoMapper.toDTO(novoTopicoCadastrado)
 
@@ -70,6 +75,7 @@ class TopicoController(
     }
 
     @PutMapping("/{id}")
+    @CacheEvict(value = ["listaDeTopicos"], allEntries = true)
     fun atualizarTopico(
             @PathVariable id: Long,
             @RequestBody topicoRequestDTO: TopicoRequestDTO
@@ -84,6 +90,7 @@ class TopicoController(
     }
 
     @DeleteMapping("/{id}")
+    @CacheEvict(value = ["listaDeTopicos"], allEntries = true)
     fun deleteTopico(@PathVariable id: Long): ResponseEntity<Unit> {
         logger.info("delete a topico $id")
 
